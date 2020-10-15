fc7005056424e88988f65488f7b1cb533c610646
Stephen <stephendanielxxx@gmail.com>
2020-10-15 16:39:56 +0700

    add launch screen, fix color name, add bottom nav bar

---
+++ Digitalent/Home/HomeTabBarViewController.swift
@@ -0,0 +1,54 @@
+//
+//  HomeTabBarViewController.swift
+//  Digitalent
+//
+//  Created by Teke on 15/10/20.
+//
+
+import UIKit
+import MaterialComponents.MaterialBottomNavigation
+
+class HomeTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let bottomNavBar = MDCBottomNavigationBar()
+        
+        bottomNavBar.delegate = self
+        bottomNavBar.titleVisibility = MDCBottomNavigationBarTitleVisibility.selected
+        bottomNavBar.alignment = MDCBottomNavigationBarAlignment.justifiedAdjacentTitles
+        
+        UITabBar.appearance().tintColor = UIColor(named: "color_2C64EE")
+        
+        let homeItem = UITabBarItem(title: "Home", image: UIImage(named: "ic_home"), tag: 0)
+        let taskItem = UITabBarItem(title: "My Class", image: UIImage(named: "ic_class"), tag: 1)
+        let profileItem = UITabBarItem(title: "Settings", image: UIImage(named: "ic_setting"), tag: 3)
+        
+        bottomNavBar.items = [homeItem, taskItem, profileItem]
+        
+        bottomNavBar.selectedItem = homeItem
+        
+        view.addSubview(bottomNavBar)
+        
+        let homeViewController = HomeViewController()
+        let taskViewController = HomeViewController()
+        let profile = HomeViewController()
+        
+        homeViewController.tabBarItem = homeItem
+        taskViewController.tabBarItem = taskItem
+        profile.tabBarItem = profileItem
+        
+        viewControllers = [homeViewController, taskViewController, profile]
+
+
+    }
+
+}
+
+extension HomeTabBarViewController: MDCBottomNavigationBarDelegate{
+    func bottomNavigationBar(_ bottomNavigationBar: MDCBottomNavigationBar, didSelect item: UITabBarItem) {
+        self.selectedViewController = viewControllers![item.tag]
+    }
+}
+
--- Podfile
+++ Podfile
@@ -4,7 +4,24 @@
 target 'Digitalent' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
-
+  pod 'Alamofire', '~> 5.2'
+  pod 'PINRemoteImage'
+  pod 'MaterialComponents/Cards'
+  pod 'MaterialComponents/BottomNavigation'
+  pod 'ImageSlideshow', '~> 1.9.0'
+  pod 'ImageSlideshow/Alamofire'
+  pod 'MaterialComponents/Tabs+TabBarView'
+  pod 'ExpandableCell'
+  pod 'Toast-Swift', '~> 5.0.1'
+  pod 'ASPVideoPlayer'
+  pod 'DropDown'
+  pod 'PopupDialog', '~> 1.1'
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Database'
+  pod 'Firebase/Crashlytics'
+  pod 'CodableFirebase'
+  pod 'SkyFloatingLabelTextField', '~> 3.0'
+  pod 'IQKeyboardManagerSwift'
   # Pods for Digitalent
 
   target 'DigitalentTests' do
