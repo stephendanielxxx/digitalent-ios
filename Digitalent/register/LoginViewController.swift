5cc6817c2157f34c8035b11be6ae737ad05fde32
Anaidyd Alcestfini <alcestfini@gmail.com>
2020-10-20 17:14:24 +0700

    fixed UI huhuuu

--- Digitalent/register/LoginViewController.swift
+++ Digitalent/register/LoginViewController.swift
@@ -9,22 +9,24 @@ import UIKit
 
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var forgetButton: UILabel!
+    @IBOutlet weak var signIn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signIn(_ sender: UIButton) {
     }
-    */
+    
+    @IBAction func signUpButton(_ sender: UIButton) {
+            let signup = SignUpViewController()
+            signup.modalPresentationStyle = .fullScreen
+            self.present(signup, animated: true, completion: nil)
+    }
+    
 
 }
