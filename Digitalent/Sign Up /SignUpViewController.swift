d5db7ce774ff33c058473afc42406c5349a16177
Stephen <stephendanielxxx@gmail.com>
2020-11-12 14:53:31 +0700

    sign up UI + API

--- Digitalent/Sign Up /SignUpViewController.swift
+++ Digitalent/Sign Up /SignUpViewController.swift
@@ -13,8 +13,6 @@ class SignUpViewController: BaseViewController {
     @IBOutlet weak var passwordIcon: UIButton!
     @IBOutlet weak var passwordIconn: UIButton!
     
-    
-    var SignupModel: SignupModel!
     var confirmPass = true
     var passClick = true
     var emailValid = false;
@@ -26,18 +24,52 @@ class SignUpViewController: BaseViewController {
     
     @IBAction func signInButton(_ sender: UIButton) {
         
-            let signin = LoginViewController()
-            signin.modalPresentationStyle = .fullScreen
-            self.present(signin, animated: true, completion: nil)
+        let signin = LoginViewController()
+        signin.modalPresentationStyle = .fullScreen
+        self.present(signin, animated: true, completion: nil)
         
     }
     @IBAction func signUpButton(_ sender: UIButton) {
-
-        let parameters: [String:Any] = [
-            "email": "\(emailText.text!)",
-            "password": "\(passwordText.text!)"
-        ]
-        postRequest(url: "user/auth/create_user", parameters: parameters, tag: "post sign up")
+        
+        let email = emailText.text!
+        let password = passwordText.text!
+        
+        if email.isEmpty{
+            let alert = UIAlertController(title: "Error", message: "Please input your email", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }else if password.isEmpty{
+            let alert = UIAlertController(title: "Error", message: "Please input your password", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }else{
+            let parameters: [String:Any] = [
+                "email": "\(emailText.text!)",
+                "password": "\(passwordText.text!)"
+            ]
+            postRequest(url: "user/auth/create_user", parameters: parameters, tag: "post sign up")
+        }
+        
+    }
+    
+    override func onSuccess(data: Data, tag: String) {
+        let decoder = JSONDecoder()
+        do {
+            let signUpModel = try decoder.decode(SignupModel.self, from: data)
+            if signUpModel.code == "200" {
+                let alert = UIAlertController(title: "Success", message: "Register success. Please check your email to activate your account.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {action in
+                    self.dismiss(animated: true, completion: nil)
+                }))
+                self.present(alert, animated: true)
+            }else{
+                let alert = UIAlertController(title: "Error", message: "\(signUpModel.message)", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+                self.present(alert, animated: true)
+            }
+        }catch{
+            print(error.localizedDescription)
+        }
     }
     
     @IBAction func passwordClick(_ sender: UIButton) {
@@ -51,11 +83,11 @@ class SignUpViewController: BaseViewController {
         }
         
         passClick = !passClick
-
+        
     }
     
     @IBAction func retypepassClick(_ sender: UIButton) {
-        if(passClick == true) {
+        if(confirmPass == true) {
             retypepassText.isSecureTextEntry = false
             passwordIconn.setImage(UIImage(systemName: "eye.slash.fill"), for:.normal)
             
