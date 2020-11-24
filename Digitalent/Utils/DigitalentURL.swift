7f5ae9b0c55e2a062d2b3a5e1b9b9653d46675bd
Stephen <stephendanielxxx@gmail.com>
2020-11-24 15:35:28 +0700

    Change url to https, Fix home online class logic

--- Digitalent/Utils/DigitalentURL.swift
+++ Digitalent/Utils/DigitalentURL.swift
@@ -8,10 +8,19 @@
 import Foundation
 
 struct DigitalentURL {
-    static let BASE_URL = "http://54.251.83.205/api_digitalent/"
-    static let URL_IMAGE_BANNER = "http://54.251.83.205/digitalent/assets.digitalent/image/"
-    static let URL_IMAGE_CLASS = "http://54.251.83.205/digitalent/assets.digitalent/image/"
-    static let URL_IMAGE_PROFILE = "http://54.251.83.205/digicourse_v2/assets.digicourse.id/user/profile_picture/"
-    static let URL_VIDEO_CLASS = "http://54.251.83.205/digitalent/assets.digitalent/video/"
-    static let URL_PDF_CLASS = "http://54.251.83.205/digitalent/assets.digitalent/pdf/"
+    //Production
+    static let BASE_URL = "https://digicourse.id/api_digitalent/"
+    static let URL_IMAGE_BANNER = "https://digicourse.id/digitalent/assets.digitalent/image/"
+    static let URL_IMAGE_CLASS = "https://digicourse.id/digitalent/assets.digitalent/image/"
+    static let URL_IMAGE_PROFILE = "https://digicourse.id/digicourse_v2/assets.digicourse.id/user/profile_picture/"
+    static let URL_VIDEO_CLASS = "https://digicourse.id/digitalent/assets.digitalent/video/"
+    static let URL_PDF_CLASS = "https://digicourse.id/digitalent/assets.digitalent/pdf/"
+    
+    //Development
+//    static let BASE_URL = "http://54.251.83.205/api_digitalent/"
+//    static let URL_IMAGE_BANNER = "http://54.251.83.205/digitalent/assets.digitalent/image/"
+//    static let URL_IMAGE_CLASS = "http://54.251.83.205/digitalent/assets.digitalent/image/"
+//    static let URL_IMAGE_PROFILE = "http://54.251.83.205/digicourse_v2/assets.digicourse.id/user/profile_picture/"
+//    static let URL_VIDEO_CLASS = "http://54.251.83.205/digitalent/assets.digitalent/video/"
+//    static let URL_PDF_CLASS = "http://54.251.83.205/digitalent/assets.digitalent/pdf/"
 }
